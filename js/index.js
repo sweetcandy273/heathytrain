3a38fc06711feb573a306ab3bf77e3a3b095e6b4
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-11-19 21:20:57 +0700

    add push page showsport

--- js/index.js
+++ js/index.js
@@ -2,10 +2,25 @@ document.addEventListener('init', function(event) {
     var page = event.target;
   
     if (page.id === 'sport') {
-      page.querySelector('#push-button').onclick = function() {
-        document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+      page.querySelector('.sport_big').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: 'Page 2'}});
       };
-    } else if (page.id === 'run') {
+      page.querySelector('.sport_small').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: 'Page 2'}});
+      };
+      page.querySelector('.sport_small').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: 'Page 2'}});
+      };
+      page.querySelector('.sport_small').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: 'Page 2'}});
+      };
+      page.querySelector('.sport_small').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: 'Page 2'}});
+      };
+      page.querySelector('.sport_small').onclick = function() {
+        document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: 'Page 2'}});
+      };
+    } else if (page.id === 'showsport') {
       page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
     }
   });
