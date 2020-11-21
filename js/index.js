919381e96e99a8cc887782b5b895df7af5317f5e
Pongsavet Phukaet <Pongsavet_name@hotmail.com>
2020-11-21 15:30:51 +0700

    show_D

--- js/index.js
+++ js/index.js
@@ -1,35 +1,19 @@
-// document.addEventListener('init', function (event) {
-//     var page = event.target;
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// var firebaseConfig = {
+//     apiKey: "AIzaSyD279kQ9_bLqaol-UqprVWn2nMmBQ8NE5A",
+//     authDomain: "heathytrain.firebaseapp.com",
+//     databaseURL: "https://heathytrain.firebaseio.com",
+//     projectId: "heathytrain",
+//     storageBucket: "heathytrain.appspot.com",
+//     messagingSenderId: "925320326632",
+//     appId: "1:925320326632:web:64253a996d5bfa38f9085d",
+//     measurementId: "G-9TLDPV2NME"
+//   };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+// var db = firebase.firestore();
 
-//     if (page.id === 'sport') {
-//         page.querySelector('#run').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'วิ่งระยะสั้น' } });
-//         };
-//         page.querySelector('#weight_high').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'ยกน้ำหนัก(ขั้นสูง)' } });
-//         };
-//         page.querySelector('#belly').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'ลดหน้าท้อง' } });
-//         };
-//         page.querySelector('#body').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'บอดี้เวท' } });
-//         };
-//         page.querySelector('#yoga').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'โยคะ' } });
-//         };
-//         page.querySelector('#weight_low').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'ยกน้ำหนัก(เริ่มต้น)' } });
-//         };
-//         page.querySelector('#dance_cover').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'เต้นโคฟเวอร์' } });
-//         };
-//         page.querySelector('#aerobic_dance').onclick = function () {
-//             document.querySelector('#myNavigator').pushPage('views/showsport.html', { data: { title: 'เต้นแอโรบิค' } });
-//         };
-//     } else if (page.id === 'showsport') {
-//         page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-//     }
-// });
 function openshowsport(id) {
     
     document.querySelector('#myNavigator').pushPage('views/showsport.html', {data: {title: id}});
@@ -39,3 +23,14 @@ function openshowsport(id) {
     document.querySelector('#myNavigator').popPage()
   
   }
+
+//   db.collection("sport").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//       if (doc.data().sport_id = "run") {
+//         const result1 =
+//           `  <img src="${doc.data().URLsport} alt="" width="70%" style="margin-top: 10px;">
+//           <p class="text_sport">${doc.data().sport_name}</p>`
+//         $("#run").append(result1)
+//       }
+//     });
+//   });
