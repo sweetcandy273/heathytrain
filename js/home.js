f9a4e420a43be9cc2937376a1295cc5279b15985
aum <sweetcandy.aum@gmail.com>
2020-11-22 20:54:07 +0700

    add pagemycouse

--- js/home.js
+++ js/home.js
@@ -8,3 +8,24 @@ function gofood() {
 function gosport() {
     window.location.href = "sport.html";
 }
+function goBack() {
+
+    document.querySelector('#navigator_home').popPage()
+
+}
+
+
+    document.addEventListener('init', function (event) {
+        var page = event.target;
+
+        if (page.id === 'home') {
+            page.querySelector('#btn_sport').onclick = function () {
+                document.querySelector('#navigator_home').pushPage('mycouse.html', { data: { title: 'My Couse' } });
+            };
+        } else if (page.id === 'mycouse') {
+            page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+
+        }
+    });
+
+
