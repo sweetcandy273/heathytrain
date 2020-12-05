d5ea97b57042a4ac52525d7b270d2cc241e9c373
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-05 21:20:49 +0700

    creat index

---
+++ js/signin.js
@@ -0,0 +1,13 @@
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            window.location.href.repeat('index.html')
+
+        } else {
+         
+        }
+      });
+
+$(function(){
+
+})
