8d47cf6601abe3af1fde7e931f82f451f8089e47
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-06 21:45:53 +0700

    add file by bee

---
+++ js/signin.js
@@ -0,0 +1,81 @@
+$(function () {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            console.log(user);
+            window.location.href = 'home.html';
+        } 
+    });
+
+    $("#signingoogle").click(function(){
+    
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function(result) {
+            if (result.credential) {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+            }
+            // The signed-in user info.
+            var user = result.user;
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            $("#error").text(errorMessage);
+          });
+
+    });
+
+    $("#signinfacebook").click(function(){
+
+      var provider = new firebase.auth.FacebookAuthProvider();
+      provider.addScope('user_birthday');
+      firebase.auth().signInWithRedirect(provider);
+
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+   
+
+      
+    });
+        
+    $("#signinemail").click(function(){
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            $("#error").text(errorMessage);
+            // ...
+          });
+    });
+
+    
+
+})
