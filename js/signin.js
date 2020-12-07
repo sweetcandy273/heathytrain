b4be0d9d6419203548a53ca15d74ea4af8ab2ef3
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-07 11:25:41 +0700

    chahe signingoogle redirec

--- js/signin.js
+++ js/signin.js
@@ -43,19 +43,22 @@ $(function () {
 
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithRedirect(provider);
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+      }
       // The signed-in user info.
       var user = result.user;
-      // ...
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(errorCode);
-      $("error").text(errorMessage);
+      $("#error").text(errorMessage);
     });
 
 
--- js/signup.js
+++ js/signup.js
@@ -47,19 +47,22 @@ var firebaseConfig = {
 
       var provider = new firebase.auth.GoogleAuthProvider();
       provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      firebase.auth().signInWithRedirect(provider);
   
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // ...
+        }
         // The signed-in user info.
         var user = result.user;
-        // ...
       }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorCode);
-        $("error").text(errorMessage);
+        $("#error").text(errorMessage);
       });
   
   
