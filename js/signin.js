f439d4683333813be4d19d5f72f581232726ffb4
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-07 10:43:34 +0700

    delete #button

--- js/signin.js
+++ js/signin.js
@@ -38,30 +38,7 @@ $(function () {
         });
   });
 
-  $('#signingoogle').click(function(){
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-    firebase.auth().signInWithRedirect(provider);
-
-    firebase.auth().getRedirectResult().then(function(result) {
-      if (result.credential) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // ...
-      }
-      // The signed-in user info.
-      var user = result.user;
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      $("#error").text(errorMessage);
-    });
-   
-
-  });
 
   $('#signinfacebook').click(function(){
     var provider = new firebase.auth.FacebookAuthProvider();
