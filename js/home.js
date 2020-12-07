40bacc6d6ece09cd3c04e35698b46a724700f8c1
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-07 18:28:33 +0700

    deploy

--- js/home.js
+++ js/home.js
@@ -30,7 +30,7 @@ $(function () {
             // $("#photo").attr("src",photoUrl);
 
         } else {
-            // window.location.href = 'signin.html';
+            window.location.href = 'firstpage.html';
         }
     });
 
@@ -47,5 +47,5 @@ function logout() {
         });
 }
 function gohome() {
-    window.location.href = "person.html";
+    window.location.href = "home.html";
 }
