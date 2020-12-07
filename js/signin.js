a1ada55d9f1153d279ffec093370788180da85b9
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-07 11:19:17 +0700

    add signin google

--- js/signin.js
+++ js/signin.js
@@ -24,18 +24,41 @@ $(function () {
     
   $("#signinemail").click(function(){
 
-      var email = $("#email").val();
-      var password = $("#password").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
 
-      firebase.auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-         
-          // ...
-        });
+    firebase.auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        $("#error").text(errorMessage);
+        
+      });
+  });
+
+  $("#signingoogle").click(function(){
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      $("error").text(errorMessage);
+    });
+
+
   });
 
 
@@ -55,11 +78,8 @@ $(function () {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
+      console.log(errorCode);
+      $("error").text(errorMessage);
     });
    
 
--- js/signup.js
+++ js/signup.js
@@ -43,6 +43,27 @@ var firebaseConfig = {
     });
 
  
+    $("#signingoogle").click(function(){
+
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        $("error").text(errorMessage);
+      });
+  
+  
+    });
     
       $('#signinfacebook').click(function(){
         var provider = new firebase.auth.FacebookAuthProvider();
