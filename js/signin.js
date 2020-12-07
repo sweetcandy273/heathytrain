a3cc0af4efd14397952298cde532b18183807af0
Orrathai Oongsakun <hat.gung.2015@gmail.com>
2020-12-07 18:07:19 +0700

    delete button facebook

--- js/signin.js
+++ js/signin.js
@@ -67,29 +67,6 @@ $(function () {
 
 
 
-  $('#signinfacebook').click(function () {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('user_birthday,email');
-
-
-
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode);
-      $("error").text(errorMessage);
-    });
-
-
-
-  });
 
 
 
